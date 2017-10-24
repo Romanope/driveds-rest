f36b14a00cc8d0630bae231b72cfc73e34c1f6e5
Carlos Romano <carlosromanno@gmail.com>
2017-10-23 23:59:34 -0200

    Implementado adição, alteração, remoção de arquivos e sincronização inicial

---
+++ controllers/controladorDiretorio.js
@@ -0,0 +1,53 @@
+var fs = require('fs');
+var path = require('path');
+var crypto = require('crypto');
+var pathDefault = "D:\\projetos\\faculdade\\2017.2\\sistemas-distribuidos\\arq-recebidos\\";
+var walk = require("walk");
+
+var controladorDiretorio = {
+	listAllFiles: function (usuario, fileName, callback) {
+
+		var pathUser = pathDefault + usuario;
+
+		if (!fs.existsSync(pathUser)){
+    		fs.mkdirSync(pathUser);
+		}
+
+		walker = walk.walk(pathUser);
+ 
+ 		var file;
+ 		var nextFile;
+ 		var fileSearched = fileName;
+
+ 		walker.on("file", function (root, fileStats, next) {
+		    
+ 			if (fileSearched == 'first') {
+ 				fileSearched = fileStats.name;
+ 			}
+
+		    var data = fs.readFileSync(pathUser + "\\" + fileStats.name);
+		    data = JSON.parse(JSON.stringify(data));
+		    
+		    if (file && (!nextFile)) {
+				nextFile = fileStats.name;							    	
+		    }
+
+		    if (fileSearched == fileStats.name) {
+		    	data.nome = fileStats.name;
+			    delete data.type;
+			    file = data;
+		    }
+	    	
+		    next();
+		});
+	  	walker.on("end", function () {
+    		callback(file, nextFile);
+  		});
+	},
+	generateHashMD5: function (data) {
+		if (!data) return null;
+		console.log('valor calculado: ' + crypto.createHash('md5').update(data).digest("hex"));
+	}
+}
+
+module.exports = controladorDiretorio;
--- routers/routers.js
+++ routers/routers.js
@@ -3,33 +3,36 @@ var router = express.Router();
 var fs = require("fs");
 var qs = require('querystring');
 var controllerUsuario = require('.././controllers/controladorUsuario');
+var controladorDiretorio = require('.././controllers/controladorDiretorio');
+var path = "D:\\projetos\\faculdade\\2017.2\\sistemas-distribuidos\\arq-recebidos\\";
 
 router.post('/', function (req, res) {
 	console.log("arquivo recebido");
 	console.log(req.body.length);
 	var arquivos = req.body;
 	for (var i = 0; i < arquivos.length; i++) {
-		writeFile(arquivos[i].usuario, arquivos[i].nome, arquivos[i].conteudo);
+		writeFile(arquivos[i].usuario, arquivos[i].nome, arquivos[i].data);
 	}
 	res.status(200).json(req.body);	
 });
 
 router.delete('/:nomeArquivo/:usuario', function (req, res) {
 	console.log("Apagando arquivo " + req.params.nomeArquivo);
-	var path = "D:\\projetos\\faculdade\\2017.2\\sistemas-distribuidos\\arq-recebidos\\"+req.params.usuario+"\\"+req.params.nomeArquivo;
-	fs.unlinkSync(path);
+	var currentPath = path + req.params.usuario + "\\" + req.params.nomeArquivo;
+	fs.unlinkSync(currentPath);
 	console.log("Arquivo " + req.params.nomeArquivo + " removido com sucesso!");
 	res.status(200).json(req.body);	
 });
 
-function writeFile(usuario, nome, conteudo) {
-	var path = "D:\\projetos\\faculdade\\2017.2\\sistemas-distribuidos\\arq-recebidos\\"+usuario;
-	if (!fs.existsSync(path)){
-    	fs.mkdirSync(path);
+function writeFile(usuario, nome, data) {
+	var currentPath = path + usuario;
+	if (!fs.existsSync(currentPath)){
+    	fs.mkdirSync(currentPath);
 	}	
-	path = "D:\\projetos\\faculdade\\2017.2\\sistemas-distribuidos\\arq-recebidos\\"+usuario+"\\"+nome;
-	var buff = new Buffer(conteudo, 'base64');
-	fs.writeFile(path, buff, 0, buff.length, 0, function(err) {
+	currentPath = path + usuario + "\\" + nome;
+
+	var buff = new Buffer(data, 'base64');
+	fs.writeFile(currentPath, buff, 0, buff.length, 0, function(err) {
 	    if(err) {
 	        console.log(err);
 	        return;
@@ -39,10 +42,41 @@ function writeFile(usuario, nome, conteudo) {
 	});
 }
 
-function getListaArquivos(usuario) {
-	fs.readdirSync(testFolder).forEach(file => {
-	  console.log(file);
+router.get('/:usuario', function (req, res) {
+	console.log('listando todos os arquivos do usuário ' + req.params.usuario);
+	var files = [];
+	var currentPath = path + req.params.usuario;
+	
+	var absolutePath;
+	fs.readdirSync(currentPath).forEach(nomeArq => {
+		absolutePath = currentPath + "\\" + nomeArq;
+		var data = fs.readFileSync(absolutePath);
+		var file = {
+			nome: nomeArq,
+			conteudo: data
+		};
+
+		files.push(file);
+
 	});
-}
+
+	res.status(200).json(files);
+});
+
+router.get('/sincronizar/:usuario/:fileName', function (req, res) {
+	console.log('consultando arquivo '+ req.params.fileName + ' do usuário ' + req.params.usuario);
+	controladorDiretorio.listAllFiles(req.params.usuario, req.params.fileName, function (result, nextFile) {
+		if (result) {
+			result.nextFile = nextFile;
+			result.emptyDirectory = false;
+		} else {
+			result = {
+				emptyDirectory: true
+			}
+		}
+		console.log('retornando arquivo ' + result.nome);
+		res.status(200).json(result).end();
+	});
+});
 
 module.exports = router;
